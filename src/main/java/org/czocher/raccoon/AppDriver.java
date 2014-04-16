2231bc7c42331953b8023ce9ed6ec55941490fff
Paweł (Czocher) Czochański <czochanski@gmail.com>
2014-04-16 13:57:23 +0200

    Handled null cases.

--- src/main/java/org/czocher/raccoon/AppDriver.java
+++ src/main/java/org/czocher/raccoon/AppDriver.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URL;
 import java.util.Locale;
 
 import org.czocher.raccoon.presenters.impl.IndexPresenterImpl;
@@ -64,24 +65,27 @@ public class AppDriver {
 
 		@Override
 		public void handle(final HttpExchange t) throws IOException {
-			final String path = t.getRequestURI().getRawPath();
+			final String uri = t.getRequestURI().getRawPath();
+			int code = 200;
 
 			String response = "No URL";
-			if (path.matches("^/" + IndexView.TAG)) {
+			if (uri.matches("^/" + IndexView.TAG)) {
 				if (indexView == null) {
 					indexView = new IndexViewImpl();
 				}
 				response = new IndexPresenterImpl(indexView).go();
-			} else if (path.matches("^/myapp/other_url")) {
-				response = "This is the response for other url";
+			} else {
+				response = "404: File not found.";
+				code = 404;
 			}
 
-			t.sendResponseHeaders(200, response.length());
+			t.sendResponseHeaders(code, response.length());
 			final OutputStream os = t.getResponseBody();
 			os.write(response.getBytes());
 
 			os.close();
 			t.close();
+			System.out.println("Request for " + uri + " handled: " + code + " " + response);
 		}
 	}
 
@@ -91,27 +95,39 @@ public class AppDriver {
 		public void handle(final HttpExchange t) throws IOException {
 			final String uri = t.getRequestURI().getRawPath();
 			final OutputStream out = t.getResponseBody();
+			final URL url = AppDriver.class.getResource(uri.substring(1));
+			final String fileNotFound = "404: File not found.";
 
-			final File resourceFile = new File(AppDriver.class.getResource(uri.substring(1)).getPath());
+			if (url == null) {
 
-			if (!resourceFile.exists() || !resourceFile.canRead()) {
-				final String fileNotFound = "404: File not found.";
 				t.sendResponseHeaders(404, fileNotFound.length());
 				out.write(fileNotFound.getBytes());
+
 			} else {
-				final byte[] buffer = new byte[1000];
-				int count = 0;
+				final File resourceFile = new File(AppDriver.class.getResource(uri.substring(1)).getPath()).getCanonicalFile();
 
-				t.sendResponseHeaders(200, 0);
+				if (!resourceFile.exists() || !resourceFile.canRead()) {
 
-				final FileInputStream ris = new FileInputStream(resourceFile);
-				while ((count = ris.read(buffer)) > 0) {
-					out.write(buffer, 0, count);
+					t.sendResponseHeaders(404, fileNotFound.length());
+					out.write(fileNotFound.getBytes());
+
+				} else {
+
+					final byte[] buffer = new byte[1000];
+					int count = 0;
+
+					t.sendResponseHeaders(200, 0);
+
+					final FileInputStream ris = new FileInputStream(resourceFile);
+					while ((count = ris.read(buffer)) > 0) {
+						out.write(buffer, 0, count);
+					}
+
+					ris.close();
 				}
-
-				ris.close();
 			}
 
+			System.out.println("Request for " + uri + " handled: " + t.getResponseCode());
 			out.close();
 			t.close();
 		}
